766d2776d59c3d6c47e02b6d754cda092119c516
xieyt01 <452198757@qq.com>
2024-02-02 10:39:16 +0800

    new  install fzf

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -24,7 +24,10 @@ return {{
 {
     "ibhagwan/fzf-lua",
     -- optional for icon support
-    dependencies = {"nvim-tree/nvim-web-devicons"},
+    dependencies = {{"nvim-tree/nvim-web-devicons"}, {
+        "junegunn/fzf",
+        build = "./install --bin"
+    }},
     config = function()
         -- calling `setup` is optional for customization
         require("fzf-lua").setup({})
